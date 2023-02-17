2bb0c90480955dcfd6b189d572b7936e86c9393e
Alex-Vdovin <av.avisto@mail.ru>
2023-02-17 14:43:14 +0300

    Реализована Сериализация

---
+++ src/main/java/org/example/IO/XSSFReader.java
@@ -0,0 +1,60 @@
+package org.example.IO;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.example.Enums.StudyProfile;
+import org.example.Model.Student;
+import org.example.Model.University;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class XSSFReader {
+    private XSSFReader() {
+
+    }
+
+    public static final List<Student> readStudents(String filePath) throws IOException {
+        List<Student> students = new ArrayList<>();
+        FileInputStream fis = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet("Студенты");
+        Iterator<Row> rows = sheet.iterator();
+        rows.next();
+
+        while (rows.hasNext()) {
+            Row currentRow = rows.next();
+            Student student = new Student();
+            student.setUniversityId(currentRow.getCell(0).getStringCellValue());
+            student.setFullName(currentRow.getCell(1).getStringCellValue());
+            student.setCurrentCourseNumber((int) currentRow.getCell(2).getNumericCellValue());
+            student.setAvgExamScore((float) currentRow.getCell(3).getNumericCellValue());
+            students.add(student);
+        }
+
+        return students;
+    }
+    public static final List<University> readUniversity(String filePath) throws IOException {
+        List<University> universities = new ArrayList<>();
+        FileInputStream fis = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet("Университеты");
+        Iterator<Row> rows = sheet.iterator();
+        rows.next();
+        while(rows.hasNext()){
+            Row currentRow = rows.next();
+            University university = new University();
+            university.setId(currentRow.getCell(0).getStringCellValue());
+            university.setFullName(currentRow.getCell(1).getStringCellValue());
+            university.setShortName(currentRow.getCell(2).getStringCellValue());
+            university.setYearOfFoundation((int) currentRow.getCell(3).getNumericCellValue());
+            university.setMainProfile(StudyProfile.valueOf(StudyProfile.class, currentRow.getCell(4).getStringCellValue()));
+            universities.add(university);
+        }
+        return universities;
+    }
+}
